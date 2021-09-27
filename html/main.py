598dd2be9420152acdf0986f99de004a0a3a551b
akovalev <you@example.com>
2021-09-27 10:40:31 +0200

    reduced stars, windows are added faster. Stats printed.

--- html/main.py
+++ html/main.py
@@ -9,10 +9,12 @@ document <= html.CANVAS(id='mycanvas', width=str(x), height=str(y),
                         style={'background-color': 'rgba(0,0,0,1)'})
 canvas=document['mycanvas']
 
-BUILDING_MIN_WIDTH=30
-BUILDING_MIN_HEIGHT=150
-SAT_WIN=0.95
-SAT_STARS=0.05
+BUILDING_MIN_WIDTH=x/20
+BUILDING_MAX_HEIGHT=y*0.7
+BUILDING_MIN_HEIGHT=20
+
+SAT_WIN=0.97
+SAT_STARS=0.01
 
 buildings=[]
 skyline=dict()
@@ -41,7 +43,9 @@ print(f"Space for stars: {space_for_stars}")
 
 stars_threshold=space_for_stars*SAT_STARS
 win_threshold=max_windows*SAT_WIN
-
+stars_killed=0
+windows_killed=0
+print(f"win: {SAT_WIN}, stars: {SAT_STARS}")
 
 
 
@@ -63,17 +67,26 @@ def addwindow(buildings, windows):
     windows.append(win)
 
 def kill(stars,windows):
-   
+    global stars_killed, windows_killed
     if len(stars)>stars_threshold:
         s=stars.pop(random.randint(0,len(stars)-1))
         s.off()
+        stars_killed+=1
    
     if len(windows)> win_threshold:
         w=windows.pop(random.randint(0,len(windows)-1))
         w.off()
+        windows_killed+=1
    
     return
 
-timer.set_interval(addstar,50, stars)
-timer.set_interval(addwindow,40,buildings,windows)
-timer.set_interval(kill,430,stars,windows)
+def print_stats():
+    
+    print(f"Stars: {len(stars)}, space {space_for_stars}, {len(stars)/space_for_stars}")
+    print(f"windows: {len(windows)}, space {max_windows}, {len(windows)/max_windows}")
+    print(f"killed {stars_killed} stars and broke {windows_killed} windows")
+
+timer.set_interval(addstar,100, stars)
+timer.set_interval(addwindow,60,buildings,windows)
+timer.set_interval(kill,50,stars,windows)
+timer.set_interval(print_stats,10000)
